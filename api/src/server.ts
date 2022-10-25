e31b2f4453b9604ed4373c9ffdd0d83bcd79dd79
aleema95 <alejandro.manouellian@gmail.com>
2022-10-25 11:46:04 -0300

    created update route

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import express, {Application, Request, Response} from 'express';
 const mongoose = require('mongoose')
 const Book = require('./models/Book')
+const bodyParser = require('body-parser');
 
 const server: Application = express();
 
@@ -12,6 +13,17 @@ mongoose
   .catch((err: any) => {
     console.log(err);
   })
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 
 server.get('/', (req: Request, res: Response) => {
@@ -56,6 +68,24 @@ server.delete('/book/:id', async (req: Request, res: Response) => {
     await Book.deleteOne({ _id: id })
 
     res.send('Success')
+  } catch (error) {
+    console.log(error);
+    res.send('Failure')
+  }
+
+})
+
+server.put('/book/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { title, author } = req.body;
+
+  try {
+    const updatedBook = await Book.updateOne({ _id: id }, {
+      title,
+      author
+    })
+
+    res.send('success')
   } catch (error) {
     console.log(error);
     res.send('Failure')
