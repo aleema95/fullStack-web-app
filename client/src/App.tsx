2fe02f5aad87060dba6c5e154e8c5129120ae5e9
aleema95 <alejandro.manouellian@gmail.com>
2022-10-25 21:08:41 -0300

    removed some unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { MouseEvent } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { getBooks } from './reducer/store/store';
 import { ActionGetAllBooks } from './reducer/actions/booksActions'
 import { useAppDispatch } from './reducer/hooks'
@@ -10,7 +10,7 @@ function App() {
   const books = useSelector(getBooks)
 
   const getAllBooksOnClick = (e: MouseEvent) => {
-    const target = e.target as HTMLButtonElement
+    // const target = e.target as HTMLButtonElement
 
     dispatch(ActionGetAllBooks())
   }
